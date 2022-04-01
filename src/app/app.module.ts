c8738bb822b582abd2791c5adf0af7d95cdd4f9f
christianpinheiro <93pinheiro@gmail.com>
2022-04-01 20:45:45 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,21 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { RouterModule } from '@angular/router';
+import { DiscComponent } from './disc/disc.component';
+import { TimerComponent } from './timer/timer.component';
+import { DiscService } from './disc.service';
+import { TimerService } from './timer.service';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot([
+    {path: '', component: AppComponent},
+    {path: 'disc', component: DiscComponent},
+    {path: 'timer', component: TimerComponent},    
+  ])
+],
+  declarations: [AppComponent, HelloComponent, DiscComponent, TimerComponent],
   bootstrap: [AppComponent],
+  providers: [DiscService, TimerService]
 })
 export class AppModule {}
